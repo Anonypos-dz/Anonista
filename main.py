1bb98e7d084f15f3bab8da2c52f1198f7e5a1935
Anonypos <anonypos@hotmail.com>
2026-04-11 22:37:53 -0400

    Updating the CLI commands

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import threading
+from colorama import Fore
 import time
 from instagrapi import Client
 import os
@@ -56,4 +57,44 @@ def list_bitches(cl) -> list:
                 bitches.append(following)
     return bitches
 
-####GUI
+####CLI
+#Text colors
+colors = {
+      "yel" : Fore.YELLOW,
+      "red" : Fore.RED,
+      "blu" : Fore.BLUE,
+      "gre" : Fore.GREEN,
+      "res" : Fore.RESET
+}
+
+#Program Logo
+anonista_logo =r"""    _                      _     _        
+   / \   _ __   ___  _ __ (_)___| |_ __ _ 
+  / _ \ | '_ \ / _ \| '_ \| / __| __/ _` |
+ / ___ \| | | | (_) | | | | \__ \ || (_| |
+/_/   \_\_| |_|\___/|_| |_|_|___/\__\__,_|
+      V1.0 BETA (CLI)"""
+help_msg = f"""   -=Programmed by Anonypos=-
+{colors['red']}_-----------------------_
+{colors["gre"]}** {colors['blu']}Login managment {colors["gre"]}**
+{colors["red"]}_-----------------------_{colors['res']}
+{colors['yel']}login{colors['res']} - {colors['blu']}Login to your Instagram account using username and password.
+{colors['yel']}sessions{colors['res']} - {colors['blu']}List all the logged sessions.
+{colors['yel']}dump_session <id> - {colors['blu']}Login by dumping a saved session.
+{colors["red"]}-______________________-
+
+{colors['res']}
+"""
+print(anonista_logo)
+print(f"Type {colors['yel']}'help'{colors["res"]} to show the commands.")
+while True:
+      try:
+            cmd = input("Anonista>> ")
+            if cmd.lower() == "exit":
+                  print(f"\nGood byeeee! {colors['red']}See u soon +-+{colors["res"]}")
+                  exit(0)
+            elif cmd.lower() == "help":
+                  print(help_msg)
+      except KeyboardInterrupt:
+            print(f"\nGood byeeee! {colors['red']}See u soon +-+{colors["res"]}")
+            break
